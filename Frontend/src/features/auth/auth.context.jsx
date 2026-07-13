1a96d173bba4668dd1e794e9bd434ae5027ba0d6
Priyabrata Mondal <mpriyabrata464@gmail.com>
2026-07-13 10:07:23 +0530

    Initial commit

---
+++ Frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+
+export const AuthContext = createContext()
+
+export const AuthProvide = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, loading, setLoading }} >
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
